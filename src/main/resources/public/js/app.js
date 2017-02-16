88147460c2435749d92327b8f524bea6c2820907
dmkato <dmkato@gmail.com>
2017-02-15 17:33:24 -0800

    Touch is working with fire and scan

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,5 +1,7 @@
 var gameModel;
+var didPressScan;
 
+/* On page ready.. */
 $( document ).ready(function() {
   // Handler for .ready() called.
   $.getJSON("model", function( json ) {
@@ -11,6 +13,7 @@ $( document ).ready(function() {
   createGameBoards();
 });
 
+/* Places Ship based on buttons that no longer exist */
 function placeShip() {
   console.log($( "#shipSelec" ).val());
   console.log($( "#rowSelec" ).val());
@@ -37,12 +40,13 @@ function placeShip() {
   });
 }
 
-function fire(){
-  console.log($( "#colFire" ).val());
-  console.log($( "#rowFire" ).val());
+/* Fires at coordinates x, y */
+function fire(x, y){
+  console.log(x);
+  console.log(y);
   //var menuId = $( "ul.nav" ).first().attr( "id" );
   var request = $.ajax({
-    url: "/fire/"+$( "#colFire" ).val()+"/"+$( "#rowFire" ).val(),
+    url: "/fire/"+x+"/"+y,
     method: "post",
     data: JSON.stringify(gameModel),
     contentType: "application/json; charset=utf-8",
@@ -61,12 +65,13 @@ function fire(){
 
 }
 
-function scan(){
-  console.log($( "#colScan" ).val());
-  console.log($( "#rowScan" ).val());
+/* Scans around coordinates x, y */
+function scan(x, y){
+  console.log(x);
+  console.log(y);
   //var menuId = $( "ul.nav" ).first().attr( "id" );
   var request = $.ajax({
-    url: "/scan/"+$( "#colScan" ).val()+"/"+$( "#rowScan" ).val(),
+    url: "/scan/"+x+"/"+y,
     method: "post",
     data: JSON.stringify(gameModel),
     contentType: "application/json; charset=utf-8",
@@ -85,20 +90,23 @@ function scan(){
 
 }
 
-
+/* Logs to console */
 function log(logContents){
   console.log(logContents);
 }
 
+/* Updates view */
 function displayGameState(gameModel){
 
-  $( '#MyBoard td'  ).css("background-color", "#84ccd6");
-  $( '#TheirBoard td'  ).css("background-color", "#84ccd6");
+  $( '#MyBoard td'  ).css("background-color", "#25383C");
+  $( '#TheirBoard td'  ).css("background-color", "#25383C");
 
-  if(gameModel.scanResult){
-    alert("Scan found at least one Ship");
-  } else {
-    alert("Scan found no Ships");
+  if(didPressScan) {
+    if(gameModel.scanResult){
+      alert("Scan found at least one Ship");
+    } else {
+      alert("Scan found no Ships");
+    }
   }
 
   displayShip(gameModel.aircraftCarrier);
@@ -128,6 +136,7 @@ function displayGameState(gameModel){
   displayShip(gameModel.submarine);
 }
 
+/* Displays ship on MyBoard */
 function displayShip(ship){
   startCoordAcross = ship.start.Across;
   startCoordDown = ship.start.Down;
@@ -147,6 +156,7 @@ function displayShip(ship){
   }
 }
 
+/* Creates grid of 10 squares for MyBoard and TheirBoard */
 function createGameBoards() {
   // Create table
   var table = $("<table>").appendTo('.gameBoard');
@@ -155,9 +165,36 @@ function createGameBoards() {
   for (var y = 1; y <= 10; y++){
     var tableRow = $("<tr id='Row" + y + "'>").appendTo(table);
     for (var x = 1; x <= 10; x++){
-        tableRow.append("<td id='" + y + "_" + x + "'>");
+        tableRow.append("<td id='" + y + "_" + x + "'></td>");
     }
     table.append("</tr>");
   }
   $("</table>").appendTo('.gameBoard');
+
+  // Make grid touchable
+  $('#TheirBoard').on("click", "td", function() {
+
+    // Parse coords
+    var coords = $(this).attr('id').split("_");
+
+    // Display Coords in footer
+    $('footer #status').text("Fired at " + coords[0] + ", " + coords[1]);
+
+    // Fire or scan Coord
+    if(didPressScan){
+      scan(coords[0], coords[1]);
+    } else {
+      fire(coords[0], coords[1]);
+    }
+  });
+}
+
+/* Is called when the user presses 'scan' button */
+function pressedScan(){
+  didPressScan = true;
+}
+
+/* Is called when the user presses 'fire' button */
+function pressedFire(){
+  didPressScan = false;
 }
